4a5ee3b2a6b9e661c37ef00b10e7abc7aed062c5
amcc <alistairmcclymont@gmail.com>
2024-04-08 16:47:17 +0100

    simplified recorder

--- sketch.js
+++ sketch.js
@@ -1,22 +1,27 @@
 // make a recorder object that can do everything
 // recorder: boolean to show status
 // record: array to store values..
-// current: setter to push object into record array
+// current: function to push object into record array
+// (this automatically has access to p5 stuff if its called from draw(), setup or anytime after p5 is in the window)
 // empty: function to empty the record array
-// ... more to add
+// ... more to add perhaps?
 
 const recorder = {
   recording: true,
   record: [],
-  set current({ frameCount, milliseconds, mouseX, mouseY }) {
+  current: function (settings = {}) {
+    // we will use the p5 object to automatically store info about time and the mouse
+    // we will also an optional settings object for custom properties.
     this.record.push({
       frameCount: frameCount,
-      milliseconds: milliseconds,
+      milliseconds: millis(),
       mouseX: mouseX,
       mouseY: mouseY,
+      settings: settings,
     });
   },
   empty: function () {
+    // reset everything, this empties the record array
     this.record.length = 0;
   },
 };
@@ -31,35 +36,36 @@ function draw() {
     background(0);
 
     fill(255);
-    console.log("bg");
     circle(mouseX, mouseY, mouseY);
 
-    recorder.current = {
-      frameCount: frameCount,
-      milliseconds: millis(),
-      mouseX: mouseX,
-      mouseY: mouseY,
-    };
+    // record the current frame, there is an optional settings object we can use
+    // recorder.current()
+    recorder.current({ fill: "blue" });
   }
 }
 
-function drawRecord(record) {
+function drawRecord() {
   if (recorder.recording) {
+    // stop the loop
     noLoop();
+    // stop the recording
     recorder.recording = false;
-    console.log("stopped");
+
+    // loop through every recorded frame and do something
     recorder.record.forEach((frame) => {
-      fill(255, 50);
+      // below line checks for something custom in settings
+      if (frame.settings.fill) fill(frame.settings.fill);
       circle(frame.mouseX, frame.mouseY, mouseY);
     });
   } else {
+    // restart recording and empty the old recording
     recorder.recording = true;
     recorder.empty();
+    // start the loop
     loop();
   }
 }
 
 function mouseClicked() {
-  console.log("click");
-  drawRecord(recorder.record);
+  drawRecord();
 }
